19d9f5f8704dc13134f8ddb7e48aeafc84f446df
13531586430@163.com <13531586430@163.com>
2021-01-04 14:05:03 +0800

    v2021.0104.1405

---
+++ test1/target/debug/deps/test1.d
@@ -0,0 +1,5 @@
+G:\rust-zrb\test1\target\debug\deps\test1.exe: src\main.rs
+
+G:\rust-zrb\test1\target\debug\deps\test1.d: src\main.rs
+
+src\main.rs:
---
+++ test1/target/debug/test1.d
@@ -0,0 +1,1 @@
+G:\rust-zrb\test1\target\debug\test1.exe: G:\rust-zrb\test1\src\main.rs
